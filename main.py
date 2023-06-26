304c7878857be57a4d9da08cf857a322ef2dfe13
fbonnier <florent.bonnier@cnrs.fr>
2023-06-27 01:00:34 +0200

    Print debug

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def get_delta_array_per_type (report_data):
 
 def compute_final_scores_1method (decision_block:dict, method_block:dict):
 
+    print (method_block)
+
     # Add Method's score to final score
     decision_block["score"] += method_block[0]["score"]
 
